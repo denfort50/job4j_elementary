c055afb6e478e2ac0a6c19f13c9b61b33c9ea2bb
denfort50 <denfort50@yandex.ru>
2021-02-08 09:07:03 +0300

    4.1. Аргументы [#467806]

--- src/ru/job4j/calculator/ArgMethod.java
+++ src/ru/job4j/calculator/ArgMethod.java
@@ -4,7 +4,6 @@ public class ArgMethod {
     public static void hello(String name) {
         System.out.println("Hello, " + name);
     }
-
     public static void main(String[] args) {
         String name = "Petr Arsentev";
         int age = 33;
